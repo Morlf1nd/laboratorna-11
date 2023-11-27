7cdaa671ea99c92334dc1ef1a958ed3fc2d5035f
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-28 00:00:28 +0200

    5 завдання

--- laboratorna-11.cpp
+++ laboratorna-11.cpp
@@ -2,32 +2,12 @@
 
 int main() {
     setlocale(0, ".1251");
-    long long number;
-    std::cout << "Введіть число: ";
-    std::cin >> number;
+    int Dobytok = 1;
+    for (int i = 1; i < 9; i++)
+    {
+        Dobytok *= (1 + (i * (i + 1)) / 2);
 
-    int one = 0;
-    int zero = 0;
-
-
-    while (number > 0) {
-
-        int chislo = number % 10;
-
-        if (chislo == 1) {
-            one++;
-        }
-        if (chislo == 0) {
-            zero++;
-        }
-
-
-        number /= 10;
     }
-
-
-    std::cout << "Кількість 1: " << one << std::endl;
-    std::cout << "Кількість 0: " << zero << std::endl;
-
+    std::cout << "Добуток= " << Dobytok;
     return 0;
 }
